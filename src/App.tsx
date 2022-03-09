2304bfb9fedda9cf98d15e10d7e0ce898b3264f2
Gabriel Santos <gabrielsantos98898@gmail.com>
2022-03-08 22:07:13 -0300

    Lista de tarefas que vai aparecer na tela pronta junto com seu style

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import * as C from './App.styles'
+import { Item } from './types/item'
+import { ListItem } from './components/ListItem'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pao', done: false },
+    { id: 2, name: 'Comprar Bolo', done: true },
+  ]); 
+
+
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de tarefas</C.Header>
+
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+
+      </C.Area>
+    </C.Container>
   );
 }
 
